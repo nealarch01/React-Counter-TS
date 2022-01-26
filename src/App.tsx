526af5b9a1d4a3ec61b09be9629f0d14aef9db7c
nealarch01 <archivalneal@gmail.com>
2022-01-25 17:06:40 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Ripples from 'react-ripples';
+
 function App() {
+  const neutralStyle = { color: 'white' }
+  const positiveStyle = { color: 'green' }
+  const negativeStyle = { color: 'red' }
+  const [currentCount, SetCurrentCount] = React.useState(0);
+  const [textStyle, SetTextStyle] = React.useState(neutralStyle);
+  const duration_s = 1000;
+  useEffect(() => {
+    if (currentCount === 0) SetTextStyle(neutralStyle);
+    else if (currentCount > 0) SetTextStyle(positiveStyle);
+    else SetTextStyle(negativeStyle);
+  }, [currentCount]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ position: 'absolute', color:'#FFFFFF' }}>React Counter</h1>
+      <div className="Container">
+        <div>
+          <h1 style={textStyle}>{currentCount}</h1>
+        </div>
+        <div className="ButtonsView">
+          <div className='btnContainer'>
+            <Ripples color={'#FFFFFF90'} during={duration_s}>
+              <button type="button" className='decrementBtn' onClick={() => SetCurrentCount(currentCount - 1)}>-</button>
+            </Ripples>
+          </div>
+          <div className='btnContainer'>
+            <Ripples color='#FFFFFF90' during={duration_s}>
+              <button className='resetBtn' onClick={() => SetCurrentCount(0)}>Reset</button>
+            </Ripples>
+          </div>
+          <div className='btnContainer'>
+            <Ripples color='#FFFFFF90' during={duration_s}>
+              <button className='incrementBtn' onClick={() => SetCurrentCount(currentCount + 1)}>+</button>
+            </Ripples>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
